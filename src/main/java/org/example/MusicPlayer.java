606f71b5a648da1fa39a8acc14a40051f111373d
xzz1p <olezhka.kirilov.97@mail.ru>
2023-02-20 19:10:02 +0300

    list

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,13 +1,37 @@
 package org.example;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
+
+    public void setMusicList(List<Music> list) {
+        this.musicList = list;
     }
 
-    public String playMusic(){
-        return "Player is "+music.getSong();
+    public MusicPlayer() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void playMusic(){
+        musicList.stream().forEach(x -> System.out.println(x.getSong()));
     }
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,9 +7,11 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("musicBean",Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-        System.out.println(musicPlayer.playMusic());
+//
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+        musicPlayer.playMusic();
         context.close();
     }
 }
